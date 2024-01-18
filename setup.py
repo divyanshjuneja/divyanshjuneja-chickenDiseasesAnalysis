352b037b73a9c5dd34e866283e96d4fc655af95d
divyanshjuneja <divyanshjuneja96@gmail.com>
2024-01-18 23:26:14 +0530

    Data ingestion added

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME ="chickenDiseasesAnalysis"
+REPO_NAME ="chickenDiseasesAnalysisR"
 AUTHOR_USER_NAME = "divyanshjuneja"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "divyanshjuneja96@gmail.com"
